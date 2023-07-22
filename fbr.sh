4700356614a4a5cb7b58339440cfec93d22647cc
heyimkir <137959127+heyimkir@users.noreply.github.com>
2023-07-22 15:19:33 -0400

    Add files via upload

--- fbr.sh
+++ fbr.sh
@@ -1,7 +1,9 @@
 # fedora boot repair menu
 echo Welcome to FBR!
-PS3='Please enter your choice: '
-options=("Repair GRUB" "Quit")
+echo FBR Version: v1.1
+echo Disclaimer: if you are running on a Live USB, CHROOT INTO YOUR HARD DRIVE FIRST
+PS3='Please enter your choice:'
+options=("Repair GRUB" "Reinstall GRUB UEFI" "Reinstall GRUB MBR" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -10,9 +12,29 @@ do
             echo "Repairing GRUB..."
             pkexec grub2-mkconfig -o /etc/grub2.cfg
             pkexec grub2-mkconfig -o /etc/grub2-efi.cfg
+            clear
             ;;
+        "Reinstall GRUB UEFI")
+            echo This will reinstall the GRUB Bootloader.
+            read -p "Press enter to continue."
+            sudo rm /boot/efi/EFI/fedora/grub.cfg
+            sudo rm /boot/grub2/grub.cfg
+            sudo dnf reinstall shim-* grub2-efi-* grub2-common
+            clear
+            ;;
+        "Reinstall GRUB MBR")
+        	echo This will reinstall the GRUB Bootloader.
+        	read -p "Press enter to continue"
+        	echo Please specify the /dev the root of your broken grub is on
+        	echo WITHOUT the number example: /dev/sda.
+        	read devnode
+        	echo Reinstalling GRUB...
+        	sudo grub2-install $devnode
+        	clear
+        	;;
+        	
         "Quit")
-            # leave the script
+            
 	    echo "Leaving FBR..."
 	    exit
 	    ;;
